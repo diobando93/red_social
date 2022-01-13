b5b6601f224667ff9e1a6e3573175a8c4e6cb108
Israel Obando <devisrael93@gmail.com>
2022-01-13 17:58:19 +0100

    manejo de usuarios 1

--- api/components/user/secure.js
+++ api/components/user/secure.js
@@ -4,7 +4,8 @@ module.exports = function checkAuth(action){
     function middleware(req, res, next){
         switch(action){
             case 'update':
-                //
+                const owner = req.body.id;
+                auth.check.own(req, owner);
                 break;
             default: 
                 next();
--- auth/index.js
+++ auth/index.js
@@ -1,8 +1,46 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const secret = config.jwt.secret;
+
+
 function sign(data){
 
-    return jwt.sign(data, 'secreto')
+    return jwt.sign(data, secret)
 
+}
+
+function verify(token){
+
+    return jwt.verify(token, secret)
+
+}
+
+const check = {
+    own: function(req, owner){
+        const decoeded = decodeHeader(req);
+        console.log(decoeded);
+    }
+}
+
+function getToken(auth){
+    if(!auth){
+        throw new Error('No viene token');
+    }
+    if(auth.indexOf('Bearer ') === -1){
+        throw new Error('Formato invalido');
+    }
+    let token = auth.replace('Bearer ', '');
+    return token;
+}
+
+function decodeHeader(req){
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decode = verify(token);
+
+    req.user = decoded;
+
+    return decoded;
 }
 
 module.exports = {
